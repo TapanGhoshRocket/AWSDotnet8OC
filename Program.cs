4cf315b3b535bdbd3bbc5d2ed7407b6164a89faa
Tapan Kumar Ghosh <40300019+TapanGhoshRocket@users.noreply.github.com>
2025-02-11 10:01:40 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@ string db = Environment.GetEnvironmentVariable("db");
 string security = Environment.GetEnvironmentVariable("security");
 Console.WriteLine("Using DB2 .NET8 provider");
 
-//using DB2Connection objConnection = new DB2Connection("Database=bludb;UID=ztf38289;PWD=0Uq0qmLFnKv4m8mo;Server=b70af05b-76e4-4bca-a1f5-23dbb4c6a74e.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud:32716;Security=ssl");
-
 //Connection String
 string connString = "uid=" + uid + ";pwd=" + pwd + ";server=" + server + ";database=" + db + ";Security=" + security;
 DB2Connection con = new DB2Connection(connString);
